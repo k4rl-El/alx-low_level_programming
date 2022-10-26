603f1bf2f09ac87275459369bbdc099546c75d68
k4rl-El <chukwuma1497@gmail.com>
2022-10-26 00:51:00 +0000

    made corrections

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,104 +1,51 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
- * listint_len - This function returns the number of elements in a linked
- * listint_t list
- * @h: The list to print
- * Return: The number of elements
+ * insert_nodeint_at_index - inserts a new node
+ * at a given position.
+ * @head: head of a list.
+ * @idx: index of the list where the new node is
+ * added.
+ * @n: integer element.
+ *
+ * Return: the address of the new node, or NULL if it
+ * failed.
  */
-
-size_t listint_len(const listint_t *h)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	size_t count = 0;
+	unsigned int i;
+	listint_t *new;
+	listint_t *h;
 
-	while (h != NULL)
+	h = *head;
+
+	if (idx != 0)
 	{
-		count++;
-		h = h->next;
+		for (i = 0; i < idx - 1 && h != NULL; i++)
+		{
+			h = h->next;
+		}
 	}
 
-	return (count);
-}
+	if (h == NULL && idx != 0)
+		return (NULL);
 
-/**
- * add_nodeint - This function adds a new node at the beginning
- * of a listint_t list
- * @head: The listint_t list
- * @n: The integer element inside the listint_t list
- * Return: The address of the new element, or NULL for failure
- */
-
-listint_t *add_nodeint(listint_t **head, int n)
-{
-	/* create a new node & allocate memory for it */
-	listint_t *new = malloc(sizeof(listint_t));
-
-	/* check to see if malloc failed */
+	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
 
-	/* assign new data */
 	new->n = n;
-	new->next = *head;
 
-	/* assigning new as the head/beginning */
-	*head = new;
-
-	return (new);
-}
-
-
-/**
- * insert_nodeint_at_index - This function inserts a new node
- * at a given position
- * @head: The double pointer to the listint_t list
- * @idx: The index of the list
- * @n: The integer ???
- * Return: The address of the new node, or NULL if it failed
- */
-
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
-{
-	/**
-	 *   algorithm:
-	 * - maintain a count and loop through all the elements in the list
-	 *   to help keep track of the index.
-	 * - once you reach the node, where you have to insert the new node
-	 *        ~ create the new node
-	 *        ~ point the next pointer of the prev node to new node
-	 *        ~ point the next pointer of the new node to current node
-	 */
-
-	unsigned int i;
-	listint_t *new, *prev, *current;
-
-	if (*head == NULL)
-		return (NULL);
-
-	current = *head;
-
-	if (idx > listint_len(*head) || idx <= 0)
-		return (NULL);
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+	}
 	else
 	{
-		if (idx == 1)
-		{
-			new = add_nodeint(head, n);
-			return (new);
-		}
-		else
-		{
-			for (i = 1; i < idx; i++)
-			{
-				prev = current;
-				current = current->next;
-			}
-			new = malloc(sizeof(listint_t));
-			new->n = n;
-			prev->next = new;
-			new->next = current;
-		}
+		new->next = h->next;
+		h->next = new;
 	}
+
 	return (new);
 }
